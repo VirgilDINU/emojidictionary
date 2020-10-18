680ac0a3f9c1956740c942ef14352d3aaf3fec33
Virgil DINU <vir_dinu@yahoo.com>
2020-10-18 13:24:59 +0300

    Emoji 18 oct 2020 13.24

---
+++ ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Virgil DINU on 18/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var emojiTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+
+}
+
